5dd279d795f3962b866511997b0146a3dc5c3d06
pimentelleo <leonardociberxon@gmail.com>
2022-02-14 10:38:18 -0300

    Populating projetc

---
+++ app.js
@@ -0,0 +1,11 @@
+const port = 3000;
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/calc', (req, res) => res.send('my name is calculator!'));
+
+app.get('/funcionarios', (req, res) => res.send("HI MY NAME IS FELYPE"));
+
+app.listen(port, () => console.log('Example app listening on port 3000!'));
