8df79d89ab6cd794b3b0a269c7a85ccacbe802c7
pimentelleo <leonardociberxon@gmail.com>
2022-02-14 11:02:18 -0300

    change port

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const port = 3000;
+const port = 80;
 const express = require('express');
 const app = express();
 
@@ -8,4 +8,4 @@ app.get('/calc', (req, res) => res.send('my name is calculator!'));
 
 app.get('/funcionarios', (req, res) => res.send("HI MY NAME IS FELYPE"));
 
-app.listen(port, () => console.log('Example app listening on port 3000!'));
+app.listen(port, () => console.log('Example app listening on port ${port}!'));
